9311f03c0aedc6a8e965383ea84cc0bcb02261d6
BrainADZ <web@brainadz.marketing>
2025-12-31 15:28:56 +0530

    changes career page added

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
 
-  images: {
-    unoptimized: true, // 🔥 VERY IMPORTANT for static export
-  },
+  // images: {
+  //   unoptimized: true, // 🔥 VERY IMPORTANT for static export
+  // },
 };
 
 export default nextConfig;
